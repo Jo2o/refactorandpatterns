b3e0714df03120c9e73c5c142a7a5c4c07dcf384
JozefMikus <jozef.mikus@kyndryl.com>
2023-02-22 10:30:30 +0100

    setup

---
+++ src/main/java/refactor/youtube/griegler/splitphase/Performance.java
@@ -0,0 +1,13 @@
+package refactor.youtube.griegler.splitphase;
+
+public class Performance {
+
+    public Play play;
+    public int audience;
+
+    public Performance(Play play, int audience) {
+        this.play = play;
+        this.audience = audience;
+    }
+
+}
---
+++ src/main/java/refactor/youtube/griegler/splitphase/Play.java
@@ -0,0 +1,13 @@
+package refactor.youtube.griegler.splitphase;
+
+public class Play {
+
+    public String name;
+    public String type;
+
+    public Play(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+}
